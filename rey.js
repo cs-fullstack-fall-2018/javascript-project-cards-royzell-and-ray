74291c922020fa639bdfd26fb1911a2b99b9f085
RoyzellW <royzellwalton@gmail.com>
2018-08-23 15:00:04 -0500

    Done

--- rey.js
+++ rey.js
@@ -1,3 +1,5 @@
+// function game1() {
+
 
 const cards = [
     { cardNumber: 6, type: 'h' },
@@ -64,125 +66,125 @@ player2Deck = player1Deck.splice(1, 26);
 console.log(player1Deck);
 console.log(player2Deck);
 
-for(let x=0; x<4; x++){
-    let player1Answer = player1Deck[x].cardNumber;
-    let player2Answer = player2Deck[x].cardNumber;
-    if(player1Answer<player2Answer){
-        player1points += 1;
-    }else{
-        player2points += 1;
-    }
+// }
 
+function shuffle() {
 
-}
+    var cards = [];
+    var typeArray = ["s", "d", "c", "h"];
+    var cardNumberArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
 
-if(player1points > player2points){
-    alert("player 1 wins")
-}else if(player2points > player1points){
-    alert("player 2 wins")
-}else{
-    alert("tie")
-}
-
-
-
-
-var cards = new Array();
-var typeArray = ["s", "d", "c", "h"];
-var cardNumberArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
-
-for(let i=0; i <typeArray.length; i++)
-{
-    for(let x=0; x <cardNumberArray.length; x++)
+    for(let i=0; i <typeArray.length; i++)
     {
-        let card = {cardNumber: cardNumberArray[x], type: typeArray[i]};
-        cards.push(card);
+        for(let x=0; x <cardNumberArray.length; x++)
+        {
+            let card = {cardNumber: cardNumberArray[x], type: typeArray[i]};
+            cards.push(card);
+        }
     }
-}
 
-for (var i=0; i < 10000; i++)
-{
-    let rand1 = Math.floor((Math.random() * cards.length));
-    let rand2 = Math.floor((Math.random() * cards.length));
-    let tmp = cards[rand1];
-    cards[rand1] = cards[rand2];
-    cards[rand2] = tmp;
-}}
+    for (var i=0; i < 10000; i++)
+    {
+        let rand1 = Math.floor((Math.random() * cards.length));
+        let rand2 = Math.floor((Math.random() * cards.length));
+        let tmp = cards[rand1];
+        cards[rand1] = cards[rand2];
+        cards[rand2] = tmp;
+    }
+
+    player1Deck = cards.slice();
+    player2Deck = player1Deck.splice(1, 26);
+
+    console.log(player1Deck);
+    console.log(player2Deck);
+
+    game();
+}
 
 
 function game() {
 
-    playerHand = cards.slice();
-    playe2hand = cards.slice();
-
-    var randomHand1 = Math.floor(Math.random() * playerHand);
-    var randomHand2 = Math.floor(Math.random() * playe2hand);
-
-
-    function Score() {
-
-        var player1Score = 0;
-
-
-        player1Score+=1;
-
-        console.log(player1Score);
-
-
-    }
-
-    function score2() {
-
-        var player2Score = 0 ;
-
-        player2Score+=1;
-
-        console.log(player2Score);
-
-    }
-
-    function winningConditions(){
-
-        if (randomHand1.shift() > randomHand2) {
-
-            var playerscore1 = 0;
-
-            playerscore1 = playerscore1 + 1;
-
-            console.log("Player1 Score: " + playerscore1);
+    for (let x = 0; x < 4; x++) {
+        let player1Answer = player1Deck[x].cardNumber;
+        let player2Answer = player2Deck[x].cardNumber;
+        if (player1Answer < player2Answer) {
+            player1points += 1;
+        } else {
+            player2points += 1;
         }
 
-        if (randomHand1.shift() < randomHand2) {
-
-            var playerscore2 = 0;
-
-            playerscore2 = playerscore2 + 1;
-
-            console.log("Player2 Score: " + playerscore2);
-        }
-
-        if (randomHand1.shift() === randomHand2) {
-
-            console.log("Draw")
-        }
-
-
     }
 
-    function Winner() {
-        //
-        // if (randomHand1.shift() > randomHand2) {
-        //     Score(player1Score, player2Score);
-        //
-        //     game();
-        //
-        // }
-        //
-        // else if (randomHand1.shift === randomHand2.shift) {
-        //
-        //     if (randomHand1.unshift() >
-        // }
-
-    }
-
+    console.log(player1points);
+    console.log(player2points);
+    result();
 }
+
+
+function result() {
+
+    if (player1points > player2points) {
+        alert("player 1 wins")
+    } else if (player2points > player1points) {
+        alert("player 2 wins")
+    } else {
+        alert("tie")
+    }
+    player1points = 0;
+    player2points = 0;
+}
+
+
+// var Deck = [];
+// var typeArray = ["s", "d", "c", "h"];
+// var cardNumberArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+//
+// for(let i=0; i <typeArray.length; i++)
+// {
+//     for(let x=0; x <cardNumberArray.length; x++)
+//     {
+//         let card = {cardNumber: cardNumberArray[x], type: typeArray[i]};
+//         Deck.push(card);
+//     }
+// }
+//
+// for (var i=0; i < 10000; i++)
+// {
+//     let rand1 = Math.floor((Math.random() * cards.length));
+//     let rand2 = Math.floor((Math.random() * cards.length));
+//     let tmp = Deck[rand1];
+//     Deck[rand1] = Deck[rand2];
+//     Deck[rand2] = tmp;
+// }
+//
+//
+//
+//
+//     function winningConditions(){
+//
+//         if (randomHand1.shift() > randomHand2.shift()) {
+//
+//             var playerscore1 = 0;
+//
+//             playerscore1 = playerscore1 + 1;
+//
+//             console.log("Player1 Score: " + playerscore1);
+//         }
+//
+//         if (randomHand1.shift() < randomHand2) {
+//
+//             var playerscore2 = 0;
+//
+//             playerscore2 = playerscore2 + 1;
+//
+//             console.log("Player2 Score: " + playerscore2);
+//         }
+//
+//         if (randomHand1.shift() === randomHand2) {
+//
+//             console.log("Draw")
+//         }
+//
+//
+//     }
+//
